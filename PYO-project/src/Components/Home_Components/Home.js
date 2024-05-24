d2edc7b9dbfd9e11cbba58b03d20c7461e1f09c6
LYH513 <peperony367@gmail.com>
2024-05-25 03:43:16 +0900

    css: 더보기 버튼 추가

--- PYO-project/src/Components/Home_Components/Home.js
+++ PYO-project/src/Components/Home_Components/Home.js
@@ -7,6 +7,7 @@ function Home (){
   return (
     <Div>
       <RecentCard/>
+      <MoreBtn>더보기</MoreBtn>
     </Div>
   );
 }
@@ -17,8 +18,32 @@ const Div =styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background: var(--blueblack-1, #34334A);
+
 `;
 
+const MoreBtn =styled.button`
+  display: flex;
+  width: 220px;
+  height: 60px;
+  padding: 21px 75px 20px 75px;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background: var(--blue, #645DFF);
+
+  color: var(--, #FFF);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-top: 100px;
+    border: none;
+    cursor: pointer;
+`;
